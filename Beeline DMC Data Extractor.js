83ea7a120355d60694db16e86e022eecac67c7ec
zOnVolga <96723108+zOnVolga@users.noreply.github.com>
2025-04-25 17:23:50 +0300

    Update Beeline DMC Data Extractor.js
    Добавлены правильные ссылки для Яндекс.
    Исправлен формат Высоты объекта

--- Beeline DMC Data Extractor.js
+++ Beeline DMC Data Extractor.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Beeline DMC Data Extractor
 // @namespace    http://tampermonkey.net/
-// @version      7.0.1
+// @version      7.0.2
 // @description  Извлечение данных о проектах и задачах после авторизации
 // @author       zOn
 // @match        https://dmc.beeline.ru/projects
@@ -552,7 +552,7 @@
         const table = document.createElement('table');
         table.setAttribute('cellspacing', '0');
         table.setAttribute('class', 'style__TableStyled-sc-1c82g3t-0 cobzxA');
-
+    
         let headers;
         if (type === 'project') {
             headers = [
@@ -574,8 +574,6 @@
                 'bs_gfk',
                 'rru_gfk',
                 'geo',
-                'route_LL', // Новый столбец: Доставка
-                'route_PPO', // Новый столбец: Обследование
                 'statuses_project',
                 'pos_code',
                 'prime_contractors',
@@ -603,7 +601,7 @@
                 'status'
             ];
         }
-
+    
         const headerRow = document.createElement('tr');
         headerRow.setAttribute('class', 'style__TableStringStyled-sc-1c82g3t-1 hwrWAV');
         headers.forEach(header => {
@@ -620,7 +618,7 @@
             headerRow.appendChild(th);
         });
         table.appendChild(headerRow);
-
+    
         data.forEach(item => {
             const row = document.createElement('tr');
             row.setAttribute('class', 'style__TableStringStyled-sc-1c82g3t-1 hwrWAV style__ProjectString-sc-hfirfp-1 hNFYXu');
@@ -630,72 +628,116 @@
                 const div = document.createElement('div');
                 div.setAttribute('class', 'style__TableCellContentStyled-sc-1c82g3t-5 ePYmXN');
                 const span = document.createElement('span');
-
+    
                 let value;
                 if (type === 'project') {
-                    if (header === 'geo') {
-                        const match = item[header]?.match(/\(([^)]+)\)/);
-                        if (match) {
-                            const coords = match[1].split(' ');
-                            const lat = coords[0];
-                            const lng = coords[1];
+                    // Обработка поля "Высота объекта"
+                    if (header === 'hight_object') {
+                        value = item[header]
+                            ? item[header].toString().replace('.', ',') // Замена "." на ","
+                            : '';
+                    } else if (header === 'geo') {
+                        const geoMatch = item[header]?.match(/\(([^)]+)\)/);
+                        if (geoMatch) {
+                            const [lat, lng] = geoMatch[1].split(' '); // Разделяем координаты
+                            const projectCoords = `${lng},${lat}`; // Меняем порядок: долгота,широта
                             const link = document.createElement('a');
-                            link.href = `https://yandex.ru/maps/?pt=${lat},${lng}&z=16&l=skl`;
-                            link.textContent = `${lat},${lng}`;
+                            link.href = `https://yandex.ru/maps/?pt=${projectCoords}&z=16&l=skl`;
+                            link.textContent = `${projectCoords}`;
                             span.appendChild(link);
                         } else {
                             span.textContent = item[header] || '';
                         }
-                    } else if (header === 'route_LL' || header === 'route_PPO') {
-                        const branchName = item.branch; // Предполагается, что в данных есть поле с названием филиала
-                        const branchCoords = branchCoordinates[branchName];
-
-                        const geoMatch = item.geo?.match(/\(([^)]+)\)/);
-                        const projectCoords = geoMatch ? geoMatch[1].split(' ').join(',') : '';
-
-                        if (header === 'route_LL') {
-                            // Логика для Доставки
-                            if (branchCoords && branchCoords.delivery && projectCoords) {
-                                const deliveryCoords = branchCoords.delivery;
-                                const url = `https://yandex.ru/maps/?rtext=${deliveryCoords}~${projectCoords}&mode=routes&routes%5Bavoid%5D=tolls%2Cunpaved%2Cpoor_condition&rtm=atm&rtt=auto&ruri=~`;
-
-                                const link = document.createElement('a');
-                                link.href = url;
-                                link.textContent = 'Доставка ->>>';
-                                link.target = '_blank';
-                                span.appendChild(link);
-                            } else {
-                                span.textContent = ''; // Если координаты отсутствуют, оставляем пустым
-                            }
-                        } else if (header === 'route_PPO') {
-                            // Логика для Обследования
-                            if (branchCoords && branchCoords.survey && projectCoords) {
-                                const surveyCoords = branchCoords.survey;
-                                const url = `https://yandex.ru/maps/?rtext=${surveyCoords}~${projectCoords}&mode=routes&routes%5Bavoid%5D=tolls%2Cunpaved%2Cpoor_condition&rtm=atm&rtt=auto&ruri=~`;
-
-                                const link = document.createElement('a');
-                                link.href = url;
-                                link.textContent = 'Обсл-е ->>>';
-                                link.target = '_blank';
-                                span.appendChild(link);
-                            } else {
-                                span.textContent = ''; // Если координаты отсутствуют, оставляем пустым
-                            }
-                        }
                     } else {
-                        span.textContent = item[header] || '';
+                        value = item[header] || '';
                     }
                 } else {
-                    span.textContent = item[header] || '';
+                    switch (header) {
+                        case 'user_position':
+                            value = item.user?.position || '';
+                            break;
+                        case 'user_short_name':
+                            value = item.user?.short_name || '';
+                            break;
+                        case 'project_number':
+                            value = item.project_bs?.project_number || '';
+                            break;
+                        case 'bs_number':
+                            value = item.project_bs?.bs_number || '';
+                            break;
+                        case 'bs_name':
+                            value = item.project_bs?.bs_name || '';
+                            break;
+                        case 'bs_address':
+                            value = item.project_bs?.bs_address || '';
+                            break;
+                        case 'project_type':
+                            value = item.project_bs?.project_type || '';
+                            break;
+                        case 'gfk':
+                            value = item.project_bs?.gfk || '';
+                            break;
+                        case 'branch':
+                            value = item.branch || '';
+                            break;
+                        case 'region':
+                            value = item.region || '';
+                            break;
+                        case 'status':
+                            value = getStatusName(item.status);
+                            break;
+                        case 'object_number':
+                            value = item.object?.number || '';
+                            break;
+                        case 'object_name':
+                            value = item.object?.name || '';
+                            break;
+                        case 'object_status_name':
+                            value = item.object?.status?.name || '';
+                            break;
+                        case 'created_at':
+                            value = item.created_at ? item.created_at.substring(0, 10) : ''; // Берем первые 10 символов (дата)
+                            break;
+                        default:
+                            value = item[header] || '';
+                            break;
+                    }
                 }
-
+    
+                if (header === 'id' || header === 'project_number') {
+                    const link = document.createElement('a');
+                    link.href = `https://dmc.beeline.ru/projects/${item[header]}`;
+                    link.textContent = value;
+                    span.appendChild(link);
+                } else if (header === 'object_number') {
+                    const link = document.createElement('a');
+                    const isNumeric = /^\d+$/.test(value);
+                    const objectName = item.object?.name || '';
+                    const objectNumber = item.object?.number || '';
+                    if (isNumeric) {
+                        if (objectName.startsWith('Зад')) {
+                            link.href = `https://dmc.beeline.ru/jobs/${item.object.id}`;
+                        } else {
+                            link.href = `https://dmc.beeline.ru/documents/${item.object.id}`;
+                        }
+                    } else if (objectNumber.startsWith('ВВР')) {
+                        link.href = `https://dmc.beeline.ru/completed-works/${item.object.id}`;
+                    } else {
+                        link.href = `https://dmc.beeline.ru/additional-agreements/${item.object.id}`;
+                    }
+                    link.textContent = value;
+                    span.appendChild(link);
+                } else {
+                    span.textContent = value;
+                }
+    
                 div.appendChild(span);
                 td.appendChild(div);
                 row.appendChild(td);
             });
             table.appendChild(row);
         });
-
+    
         return table.outerHTML;
     }
 
