03c863ea4f89c32a4fbca4f2392e2e02660d08c1
zOnVolga <96723108+zOnVolga@users.noreply.github.com>
2025-05-23 15:55:44 +0300

    Update Beeline_DMC_Data_Extractor.js

--- Beeline_DMC_Data_Extractor.js
+++ Beeline_DMC_Data_Extractor.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Beeline DMC Data Extractor + AutoUpdater
 // @namespace    http://tampermonkey.net/
-// @version      7.3.0
+// @version      7.3.1
 // @description  Извлечение данных из Beeline DMC с возможностью автообновления и уведомлением о последнем коммите
 // @author       zOnVolga
 // @match        https://dmc.beeline.ru/*
